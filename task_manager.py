1680692928c84b0b742c1e687250022c4546da69
Patrik <patrikhrmo@outlook.com>
2025-06-10 11:52:15 +0200

    stage_2

--- task_manager.py
+++ task_manager.py
@@ -5,9 +5,11 @@ ukoly = [
     }
     ]
 
+
 def pridat_ukol():
+
     while True:
-        x = input("Zadejte název úkolu: ")
+        x = input("\nZadejte název úkolu: ")
         if x == "":
             print("Prázdný vstup.")
             continue
@@ -18,32 +20,67 @@ def pridat_ukol():
             print("Prázdný vstup.")
             continue
         break
+
     ukoly[0]["ukol"].append(x)
     ukoly[0]["popis"].append(y)
-   
+
     for item in range(len(ukoly[0]["ukol"])):
         print(f"Úkol \"{x}\" byl přidán.")
        
     hlavni_menu()
 
+
 def zobrazit_ukoly():
-    for item in range(len(ukoly[0]["ukol"])):
-        print(ukoly[0]["ukol"][item])
-        print(ukoly[0]["popis"][item])
+
+    if ukoly[0]["ukol"] == []:
+        print("\nNemáte žádné úkoly.")
+    else:
+        print("\nSeznam úkolů:")
+        for item in range(len(ukoly[0]["ukol"])):
+            print(f"{item+1}. {ukoly[0]["ukol"][item]} - {ukoly[0]["popis"][item]}")
+
     hlavni_menu()
 
+
 def odstranit_ukol():
-    print("Something")
+
+    if ukoly[0]["ukol"] == []:
+        print("\nNemáte žádné úkoly.")
+        hlavni_menu()
+    else:
+        print("\nSeznam úkolů:")
+        for item in range(len(ukoly[0]["ukol"])):
+            print(f"{item+1}. {ukoly[0]["ukol"][item]} - {ukoly[0]["popis"][item]}")
+
+        while True:
+            try:
+                odstraneny_ukol = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
+            except ValueError:
+                continue
+            if odstraneny_ukol <= 0:
+                print("Nesprávny vstup.")
+                continue
+            break
+        z = ukoly[0]["ukol"][odstraneny_ukol - 1]
+        del ukoly[0]["ukol"][odstraneny_ukol - 1]
+        del ukoly[0]["popis"][odstraneny_ukol - 1]
+        print(f"Úkol \"{z}\" byl odstraněn.")
+
+        hlavni_menu()
+
 
 def konec_programu():
-    print("Konec")
+    print("\nKonec programu.")
+
 
 def hlavni_menu():
+
     print("\nSprávce úkolů - Hlavní menu")
     print("1. Přidat nový úkol")
     print("2. Zobrazit všechny úkoly")
     print("3. Odstranit úkol")
     print("4. Konec programu")
+
     while True:
         try:
             moznost = int(input("Vyberte možnost (1-4): "))
